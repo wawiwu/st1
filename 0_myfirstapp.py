31c2c354b99450ee0884b8007ed8b05a851ab75c
wawiwu <113175807+wawiwu@users.noreply.github.com>
2022-09-09 17:46:32 +0800

    Add files via upload

---
+++ 0_myfirstapp.py
@@ -0,0 +1,11 @@
+### import library
+import streamlit as st
+
+
+### write your app
+st.write("Good morning.")
+
+### change to Hello there
+### click always rerun for automatic update
+
+### change to Good morning
